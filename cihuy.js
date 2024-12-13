d78e6f1179aa075a97da38d2cc65cd6d72b63df4
AdityaSutanto <aditya.422024013@civitas.ukrida.ac.id>
2024-12-13 15:09:40 +0700

    Connect to other sites

--- cihuy.js
+++ cihuy.js
@@ -2,29 +2,6 @@ document.getElementById('learn-more').addEventListener('click', () => {
   alert('Terima kasih telah tertarik! Informasi lebih lanjut akan segera hadir.');
 });
 
-// Mendapatkan elemen modal dan tombol close
-const modal = document.getElementById('profile-modal');
-const closeModal = document.getElementById('close-modal');
-const profileIcon = document.getElementById('profile-icon');
-
-// Menampilkan modal saat klik ikon profil
-profileIcon.addEventListener('click', function(event) {
-event.preventDefault();
-modal.style.display = 'flex';
-}); 
-
-// Menutup modal saat klik tombol close
-closeModal.addEventListener('click', function() {
-modal.style.display = 'none';
-});
-
-// Menutup modal jika klik di luar konten modal
-window.addEventListener('click', function(event) {
-if (event.target === modal) {
-  modal.style.display = 'none';
-}
-});
-
 // Mendapatkan elemen modal, tombol close, dan tombol pencarian
 const searchModal = document.getElementById('search-modal');
 const closeSearchModal = document.getElementById('close-search-modal');
