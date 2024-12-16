2629e6b8f0b6ff29c4acdb89240ba41458b81535
AdityaSutanto <aditya.422024013@civitas.ukrida.ac.id>
2024-12-16 15:10:31 +0700

    Login

--- shop-cart.js
+++ shop-cart.js
@@ -110,3 +110,26 @@ document.getElementById('check-out').addEventListener('click', function () {
     // Optional: Reload halaman jika ingin mereset sepenuhnya
     // location.reload();
 });
+
+// Mendapatkan elemen modal, tombol close, dan tombol pencarian
+const searchModal = document.getElementById('search-modal');
+const closeSearchModal = document.getElementById('close-search-modal');
+const searchIcon = document.getElementById('search-icon');
+
+// Menampilkan modal search saat klik ikon search
+searchIcon.addEventListener('click', function(event) {
+    event.preventDefault();
+    searchModal.style.display = 'flex';  // Menampilkan modal pencarian
+});
+
+// Menutup modal pencarian saat klik tombol close
+closeSearchModal.addEventListener('click', function() {
+    searchModal.style.display = 'none';  // Menyembunyikan modal pencarian
+});
+
+// Menutup modal pencarian jika klik di luar area modal
+window.addEventListener('click', function(event) {
+    if (event.target === searchModal) {
+        searchModal.style.display = 'none';  // Menyembunyikan modal pencarian
+    }
+});
